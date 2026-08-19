b2f3479219e9fd70316badc6a8ad216af78a82d4
thaisonnguyen123 <kimlasat1998@gmail.com>
2026-08-19 22:32:28 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+let person = {
+    name : 'Nguyễn Văn A',
+    age : 20,
+    add : 'Hà Tĩnh',
+    phone : '0123456789',
+};
+console.log('Tên:',person.name);
+console.log('tuổi:',person.age);
+console.log('Địa chỉ:',person.add);
+console.log('số điện thoại:',person.phone);
