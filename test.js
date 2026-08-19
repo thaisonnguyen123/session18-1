b09467938133492d9ea0deae065dea11fc71ed51
thaisonnguyen123 <kimlasat1998@gmail.com>
2026-08-19 22:40:13 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+let person = {
+    id : 1,
+    name : 'Nguyễn Văn A',
+    age : 20,
+    add : 'Hà Tĩnh',
+    phone : '0123456789',
+    mark : 'Nam',
+};
+
+let newstudent = {
+    id : 2,
+    name : 'Nguyễn Văn B',
+    age : 18,
+    add : 'Hải Phòng',
+    phone : '987654321',
+    mark : 'Nữ',
+};
+
+let students = [];
+
+students.push(person);
+students.push(newstudent);
+
+console.log(students);
